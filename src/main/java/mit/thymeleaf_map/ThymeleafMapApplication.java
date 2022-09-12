40490444a7680c69f8ceaf952ed8b782109ea075
Muhammadjafar <ibook2025com@gmail.com>
2022-09-12 11:09:11 +0500

    initial _001_

---
+++ src/main/java/mit/thymeleaf_map/ThymeleafMapApplication.java
@@ -0,0 +1,13 @@
+package mit.thymeleaf_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafMapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mit/thymeleaf_map/ThymeleafMapApplicationTests.java
@@ -0,0 +1,13 @@
+package mit.thymeleaf_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
